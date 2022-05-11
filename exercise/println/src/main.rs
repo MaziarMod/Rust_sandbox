f59fe9edbec04f5402f26a52f6aa6070218e2601
Maziar <Maziar.mod@gmail.com>
2022-05-11 12:14:21 -0400

    edit println exercise

--- exercise/println/src/main.rs
+++ exercise/println/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
+    //print to console
     println!("Rust Programming...");
+
+    //Basic formating
+    println!("{} is cool and {} should go to the park", "Today", "she" );
+
+    //Positional arrguments
+    println!("{0} is cool and {1} should go to the park {0}", "Today", "she" );
+
+    //Named arguments
+    println!("{project} project will be finished {date}", project = "Go to the moon", date = "May 11, 2022");
+
+    //Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Oct: {:o}", 25, 25, 25);
+
+    //placeholder for debug traits
+    println!("{:?}", ("test", false, 16));
+
+    //Basic math
+    println!("120 * 86 = {}", 120 * 86);
+
 }
